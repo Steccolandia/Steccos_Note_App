cabab7233264ca43d74e5b5ce40d611fbbd7d0e4
Stecco <steccolandia@live.it>
2023-12-15 11:07:00 +0900

    Added Test Ad Banner, added textCapSentences in the textFields. Set AdjustResize focus on Manifest file

--- app/src/main/java/com/steccos/mynoteapp/MainActivity.kt
+++ app/src/main/java/com/steccos/mynoteapp/MainActivity.kt
@@ -5,15 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView : AdView
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //next line to enable the ads
+        MobileAds.initialize(this) {}
+
+        //next block sets and launches it
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.navHostFragment) as NavHostFragment
